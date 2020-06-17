8e23c642196b398a23180607807fdbfc588f5351
BobyZhang <zsb0213@gmail.com>
2020-06-17 16:04:05 +1000

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomaPage from './homepage.component';
+import HomaPage from './pages/homepage/homepage.component';
 
 function App() {
   return (
