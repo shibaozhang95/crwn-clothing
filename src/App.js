9dd1bd67a599a2cd6a989af4508aee390f1a7891
BobyZhang <zsb0213@gmail.com>
2020-07-01 15:36:22 +1000

    finishing sign in handleSumbmit method

--- src/App.js
+++ src/App.js
@@ -33,9 +33,8 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => {
-            console.log(this.state);
           });
+          console.log(this.state)
         });
         
       }
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -17,12 +17,17 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({
+    const { email, password } = this.state;
 
-    })
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   handleChange = event => {
@@ -58,7 +63,10 @@ class SignIn extends React.Component {
           />
 
           <div className='buttons'>
-            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton 
+              type='submit'
+              onClick={this.handleSubmit}
+            > Sign in </CustomButton>
             <CustomButton 
               onClick={signInWithGoogle}
               isGoogleSignIn
