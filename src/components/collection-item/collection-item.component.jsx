148a2d7e03d7cba07a11c97b8dd32cf8f0db0220
BobyZhang <zsb0213@gmail.com>
2020-07-05 21:13:21 +1000

    introduced styled in components, changed in custom button and header

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -20,7 +20,9 @@ const CollectionItem = ({ item, addItem }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <CustomButton inverted
+      <CustomButton 
+        className='custom-button'
+        inverted
         onClick={() => addItem(item)}
       > Add to cart </CustomButton>
     </div>
