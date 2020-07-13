e91f9ebddd869c8d45710d0ad073e450a8bb72d5
BobyZhang <zsb0213@gmail.com>
2020-07-13 15:16:29 +1000

    optimised app with lazy and Suspense for homepage and shop page

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import { default as CollectionsOverview} from '../../components/collections-overview/collections-overview.container';
-import { default as CollectionPage } from '../collection/collection.container';
+// import { default as CollectionsOverview} from '../../components/collections-overview/collections-overview.container';
+// import { default as CollectionPage } from '../collection/collection.container';
+
+import Spinner from '../../components/spinner/spinner.component';
+
+const CollectionsOverview = lazy(() => import('../../components/collections-overview/collections-overview.container'));
+const CollectionPage = lazy(() => import('../collection/collection.container'));
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
@@ -14,13 +19,16 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
   return (
     <div className='shop-page'>
-      <Route exact path={`${match.path}`} 
-        component={CollectionsOverview}
-      />
-      <Route 
-        path={`${match.path}/:collectionId`} 
-        component={CollectionPage}
-      />
+      <Suspense fallback={<Spinner />}>
+        <Route exact path={`${match.path}`} 
+          component={CollectionsOverview}
+        />
+        <Route 
+          path={`${match.path}/:collectionId`} 
+          component={CollectionPage}
+        />
+      </Suspense>
+      
     </div>
   );
   
